a9aef3cb6c8bec9ecf426f1dba89e2a9865da7cf
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-06-24 19:39:26 +0100

    make compatible with Active Support

--- lib/structure/json.rb
+++ lib/structure/json.rb
@@ -1,3 +1,5 @@
+require 'structure'
+
 unless Object.const_defined?(:JSON) and ::JSON.const_defined?(:JSON_LOADED) and
   ::JSON::JSON_LOADED
   require 'json'
@@ -14,5 +16,28 @@ class Structure
     { JSON.create_id => klass }.
       merge(@attributes).
       to_json(*args)
+  end
+end
+
+if defined? ActiveSupport
+  class Structure
+    def as_json(options = nil)
+      # create a subset of the attributes by applying :only or :except
+      subset = if options
+        if attrs = options[:only]
+          @attributes.slice(*Array.wrap(attrs))
+        elsif attrs = options[:except]
+          @attributes.except(*Array.wrap(attrs))
+        else
+          @attributes.dup
+        end
+      else
+        @attributes.dup
+      end
+
+      klass = self.class.name
+      { JSON.create_id => klass }.
+        merge(subset)
+    end
   end
 end
