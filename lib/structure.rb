662527e422f5e4648cc3aad9ac71f14381a66f51
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-07-25 17:30:50 +0100

    edit class description

--- lib/structure.rb
+++ lib/structure.rb
@@ -11,7 +11,7 @@ unless Object.const_defined?(:Boolean)
   class FalseClass; include Boolean; end
 end
 
-# A Struct-like data container.
+# A key/value container for modeling ephemeral data.
 class Structure
   include Enumerable
 
