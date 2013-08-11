2322b3de66822a8d4626d576d1e84260389790e3
hakanensari <hakan.ensari@papercavalier.com>
2013-08-11 03:54:31 +0300

    Move example out of test class

--- test/structure_test.rb
+++ test/structure_test.rb
@@ -1,25 +1,25 @@
 require 'minitest/autorun'
 require 'structure'
 
-class StructureTest < MiniTest::Test
-  class Location
-    include Structure
+class Location
+  include Structure
 
-    attr :res
+  attr :res
 
-    def initialize(res)
-      @res = res
-    end
-
-    value :latitude do
-      res.fetch(:lat)
-    end
-
-    value :longitude do
-      res.fetch(:lng)
-    end
+  def initialize(res)
+    @res = res
   end
 
+  value :latitude do
+    res.fetch(:lat)
+  end
+
+  value :longitude do
+    res.fetch(:lng)
+  end
+end
+
+class StructureTest < MiniTest::Test
   def setup
     @location = Location.new(lat: 10, lng: 100)
   end
